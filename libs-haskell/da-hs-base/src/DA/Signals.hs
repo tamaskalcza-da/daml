3f98670eaf4988b03bec29abb73bd9befe8f6ec0
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2020-01-27 16:34:11 +0100

    Shut down when stdin is closed.
    changelog_begin
    
    - [DAML Assistant] The DAML assistant will now shut down when stdin is
      closed. This is mainly useful on Windows, where process APIs often
      kill the process in a way that does not allow it to do any cleanup, in
      particular, we cannot stop child processes.
    
    changelog_end

--- libs-haskell/da-hs-base/src/DA/Signals.hs
+++ libs-haskell/da-hs-base/src/DA/Signals.hs
@@ -6,13 +6,17 @@
 -- | Module that turns SIGTERM into a UserInterrupt exception in Unix.
 module DA.Signals
     ( installSignalHandlers
+    , withCloseOnStdin
     ) where
+
+import Control.Exception
+import Control.Concurrent.Async
+import System.IO
+import Control.Monad
+import Control.Concurrent
 
 #ifndef mingw32_HOST_OS
 import System.Posix.Signals
-import Control.Exception
-import Control.Concurrent
-import Control.Monad
 #endif
 
 -- | Turn SIGTERM into a UserInterrput exception in Unix. Does nothing on Windows.
@@ -25,3 +29,13 @@ installSignalHandlers = do
     mainThread <- myThreadId
     void $ installHandler sigTERM (Catch $ throwTo mainThread UserInterrupt) Nothing
 #endif
+
+withCloseOnStdin :: IO a -> IO a
+withCloseOnStdin a = do
+    mainThread <- myThreadId
+    -- On Windows we probably need to close stdin explicitely to make sure that
+    -- isEOF does not block forever.
+    withAsync (go mainThread) (const $ a `finally` hClose stdin)
+  where go mainThread = forever $ do
+            b <- isEOF
+            when b $ throwTo mainThread UserInterrupt
